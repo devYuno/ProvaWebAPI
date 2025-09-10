2d56c39df3b1d3f5846cbaffd941e322d87c664f
devYuno <annykessy20@gmail.com>
2025-09-10 16:59:51 -0300

    Finalmente terminei

---
+++ Query.sql
@@ -0,0 +1,14 @@
+-- CREATE DATABASE WebTuor
+-- USE WebTuor
+
+INSERT INTO [User] VALUES (newid(),'Kessyane','kessy','senha123');
+INSERT INTO [User] VALUES (newid(),'Fulano','fulaninho','senha123');
+INSERT INTO [User] VALUES (newid(),'Oto','otocara','senha123');
+
+INSERT INTO PontoTuristico VALUES (newid(),'Jardim Botanico');
+INSERT INTO PontoTuristico VALUES (newid(),'Passauna');
+INSERT INTO PontoTuristico VALUES (newid(),'Largo da Ordem');
+INSERT INTO PontoTuristico VALUES (newid(),'Um lugar aí');
+INSERT INTO PontoTuristico VALUES (newid(),'Oto lugar ali');
+INSERT INTO PontoTuristico VALUES (newid(),'Museu do olho');
+
